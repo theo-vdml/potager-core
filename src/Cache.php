1a802752480c8cc1e9905f6c2255558d83e6e5b4
Théo Vandormael <theo.vandormael@proton.me>
2025-06-22 20:28:27 +0200

    feat: Cache now default to sys_temp_dir when no cache folder configured

--- src/Cache.php
+++ src/Cache.php
@@ -2,34 +2,71 @@
 
 namespace Potager;
 
+use RuntimeException;
+
 class Cache
 {
-    public static function set(string $key, mixed $data, int $duration = 86400)
-    {
-        $cacheFile = path("/storage/.cache/{$key}.json");
+    protected static string $cachePath;
 
+    public static function configure(?string $cachePath = null): void
+    {
+        self::$cachePath = self::resolveCachePath($cachePath, 'cache');
+    }
+
+    protected static function resolveCachePath(?string $path, string $namespace): string
+    {
+        if ($path === null) {
+            $path = sys_get_temp_dir() . "/potager-{$namespace}";
+            @mkdir($path, 0775, true);
+            return $path;
+        }
+
+        if (!is_dir($path)) {
+            throw new RuntimeException("Provided cache path [$path] does not exist.");
+        }
+
+        if (!is_writable($path)) {
+            throw new RuntimeException("Provided cache path [$path] is not writable.");
+        }
+
+        return rtrim($path, '/');
+    }
+
+    public static function set(string $key, mixed $data, int $duration = 86400): void
+    {
+        $file = self::getCacheFilePath($key);
         $content = [
-            "expire" => time() + $duration,
-            "data" => $data
+            'expire' => time() + $duration,
+            'data' => $data
         ];
 
-        file_put_contents($cacheFile, json_encode($content));
+        file_put_contents($file, json_encode($content));
     }
 
     public static function get(string $key)
     {
-        $cacheFile = path("/storage/.cache/{$key}.json");
-        if (!file_exists($cacheFile))
+        $file = self::getCacheFilePath($key);
+
+        if (!file_exists($file)) {
             return null;
+        }
 
-        $content = file_get_contents($cacheFile);
-        $json = json_decode($content, true);
+        $json = json_decode(file_get_contents($file), true);
 
-        if ($json['expire'] < time()) {
-            unlink($cacheFile);
+        if (!$json || ($json['expire'] ?? 0) < time()) {
+            @unlink($file);
             return null;
         }
 
         return $json['data'];
+    }
+
+    protected static function getCacheFilePath(string $key): string
+    {
+        if (!isset(self::$cachePath)) {
+            self::configure(); // lazy default to tmp
+        }
+
+        return self::$cachePath . "/{$key}.json";
     }
 }
